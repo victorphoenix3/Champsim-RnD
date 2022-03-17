3b24caf30cb85bb38d37635b5a144dfd5b57d8c0
smn98 <rkz.suman@gmail.com>
2022-03-17 19:36:48 +0530

    added script: all stats

--- run_shortcut.sh
+++ run_shortcut.sh
@@ -13,7 +13,7 @@
 ./run_suite.sh bimodal-no-ipcp_isca2020-ipcp_isca2020-no-no-no-no-lru-lru-lru-drrip-lru-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach/high_dram_bw/ipcp/lru 50 50
 ./run_suite.sh bimodal-no-ipcp_isca2020-ipcp_isca2020-no-no-no-no-lru-lru-lru-drrip-drrip-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach_all_traces/ipcp/drrip 50 50
 ./run_suite.sh bimodal-no-ipcp_isca2020-ipcp_isca2020-no-no-no-no-lru-lru-lru-drrip-mockingjay-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach_all_traces/ipcp/mockingjay 50 50
-./run_suite.sh bimodal-no-ipcp_isca2020-ipcp_isca2020-no-no-no-no-lru-lru-lru-drrip-ship++-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach_all_traces/ipcp/ship++ 50 50
+./run_suite.sh bimodal-no-ipcp_isca2020-ipcp_isca2020-no-no-no-no-lru-lru-lrusumon123-drrip-ship++-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach_all_traces/ipcp/ship++ 50 50
 ./run_suite.sh bimodal-no-ipcp_isca2020-ipcp_isca2020-no-no-no-no-lru-lru-lru-drrip-hawkeye-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach_all_traces/ipcp/hawkeye 50 50
 
 ./run_suite.sh bimodal-no-bingo_dpc3-bingo_dpc3-no-no-no-no-lru-lru-lru-drrip-lru-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach_neg_all_traces/bingo/drrip 50 50
@@ -32,3 +32,6 @@
 
 ./run_suite.sh bimodal-no-no_DRAMIOFREQDIV4-no-no-no-no-no-lru-lru-lru-drrip-lru-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach/low_dram_bw/1600/no_pref/lru 50 50
 ./run_suite.sh bimodal-no-no_DRAMIOFREQDIV8-no-no-no-no-no-lru-lru-lru-drrip-lru-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach/low_dram_bw/800/no_pref/lru 50 50
+
+./run_suite.sh bimodal-no-no_DRAMIOFREQDIV4-spp-no-no-no-no-lru-lru-lru-drrip-lru-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach/low_dram_bw/1600/spp/lru 50 50
+./run_suite.sh bimodal-no-no_DRAMIOFREQDIV8-spp-no-no-no-no-lru-lru-lru-drrip-lru-lru-lru-lru-1core ../../biswa/SPEC2017Traces stats/window_approach/low_dram_bw/800/spp/lru 50 50
